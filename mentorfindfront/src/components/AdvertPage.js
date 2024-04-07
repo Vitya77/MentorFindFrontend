a13ed51db144bff292a8136acd009a1945cebacf
IvanFranCOCK <49644012+Vitya77@users.noreply.github.com>
2024-04-07 18:33:41 +0300

    Add a template of advertisement page

---
+++ mentorfindfront/src/components/AdvertPage.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import config from '../config';
+import MiniAdvert from './MiniAdvert';
+import { useLocation } from 'react-router-dom';
+
+const serverURL = config.serverURL;
+
+function AdvertPage() {
+    
+    return (
+        <div>
+            
+        </div>
+    );
+}
+    
+export default AdvertPage;
